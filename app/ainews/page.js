74931652a92d33446a3d3bf33370b11c19ae1a58
IamTarunG <tarungudipalli5@gmail.com>
2023-09-07 13:50:43 +0530

    SSR (force-dynamic)

--- app/ainews/page.js
+++ app/ainews/page.js
@@ -1,25 +1,33 @@
-
-
-import Loader from "@/components/Loader";
-import AinewsComponent from "@/components/ainews-component";
-import { Suspense } from "react";
+import CardComponent from "@/components/card";
+import { db } from "@/firebase/config";
+import { collection, getDocs } from "firebase/firestore";
 export const metadata = {
   title: "Xploring AI World | AI News",
-  description: "Xploring AI World | Stay informed with the latest AI news from around the world. Get insights into breakthroughs, research, and advancements in artificial intelligence. Stay ahead in the dynamic field of AI by exploring our comprehensive coverage of AI-related topics and developments."
-}
+  description:
+    "Xploring AI World | Stay informed with the latest AI news from around the world. Get insights into breakthroughs, research, and advancements in artificial intelligence. Stay ahead in the dynamic field of AI by exploring our comprehensive coverage of AI-related topics and developments.",
+};
 
-function AINews() {
+export const dynamic = "force-dynamic";
 
+async function AINews() {
+  const posts = [];
+  const querySnapshot = await getDocs(collection(db, "ainews"));
+  querySnapshot.forEach((doc) => {
+    const data = doc.data();
+    data.id = doc.id;
+    posts.push(data);
+  });
 
   return (
     <div className="min-h-screen">
-
       <p className="text-center text-xl">AI News</p>
-      <Suspense fallback={<Loader />}>
-
-        <AinewsComponent />
-      </Suspense>
-
+      {posts.length > 0 ? (
+        <CardComponent posts={posts} />
+      ) : (
+        <div className="flex justify-center items-center min-h-screen">
+          <p className="text-lg">No posts to show</p>
+        </div>
+      )}
     </div>
   );
 }
--- app/technews/page.js
+++ app/technews/page.js
@@ -1,27 +1,35 @@
 
-
-
-import Loader from "@/components/Loader";
-import TechNewsComponent from "@/components/technews-component";
-import { Suspense } from "react";
+import CardComponent from "@/components/card";
+import { db } from "@/firebase/config";
+import { collection, getDocs } from "firebase/firestore";
 export const metadata = {
     title: "Xploring AI World | Tech News",
     description: "Explore the latest in technology news and trends that are shaping our digital world. From gadgets and innovations to software updates and industry shifts, our tech news coverage keeps you informed about the fast-paced landscape of technology."
 }
 
-async function Technews() {
+export const dynamic = "force-dynamic";
 
+async function TechNews() {
+  const posts = [];
+  const querySnapshot = await getDocs(collection(db, "technews"));
+  querySnapshot.forEach((doc) => {
+    const data = doc.data();
+    data.id = doc.id;
+    posts.push(data);
+  });
 
-
-    return (
-        <div className="min-h-screen">
-            <p className="text-center text-xl">Tech News</p>
-            <Suspense fallback={<Loader />}>
-
-                <TechNewsComponent />
-            </Suspense>
+  return (
+    <div className="min-h-screen">
+      <p className="text-center text-xl">Tech News</p>
+      {posts.length > 0 ? (
+        <CardComponent posts={posts} />
+      ) : (
+        <div className="flex justify-center items-center min-h-screen">
+          <p className="text-lg">No posts to show</p>
         </div>
-    );
+      )}
+    </div>
+  );
 }
 
-export default Technews;
+export default TechNews;

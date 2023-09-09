13594b700cd131b2899cae3160f6a4b680976165
IamTarunG <tarungudipalli5@gmail.com>
2023-09-09 17:37:17 +0530

    Changed Loading Label

--- app/loading.js
+++ app/loading.js
@@ -4,7 +4,7 @@ import { Spinner } from "@nextui-org/react";
 function Loader() {
     return (
         <div className="flex items-center justify-center min-h-screen">
-            <Spinner label="Loading..." color="primary" labelColor="primary" />
+            <Spinner label="Hold on..." color="primary" labelColor="primary" />
         </div>
     );
 }
